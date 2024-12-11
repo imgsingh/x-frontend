1358dc6ba1f877a25266c73b67e92b4e06f98ea0
anushachengi <anusha.chengi@inttent.ai>
2024-12-11 16:13:20 +0000

    merge fix

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -22,40 +22,6 @@ function Login() {
     return emailPattern.test(email);
   }
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    if (validateEmail(username) && password !== '') {
-      fetch("http://twitter-team-turning-testers-19648cf420b7.herokuapp.com/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          "email": username,
-          "password": password,
-        })
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          const expirationDate = new Date();
-          expirationDate.setFullYear(expirationDate.getFullYear() + 20);
-          let cookiessss = getCookie("token")
-          if (cookiessss == null) {
-            document.cookie = `token=${data.token}; expires=${expirationDate.toUTCString()}; path=/`;
-
-  //           localStorage.setItem("userDetails", JSON.stringify({
-  //             userId: data.userId,
-  //             name: data.name,
-  //             email: data.email,
-  //             profileBio: data.profileBio
-  //           }));
-  //         }
-  //         navigate('/home')
-  //       })
-  //       .catch((error) => console.error("Error calling API:", error));
-  //   } else {
-  //     alert("Invalid Credentials")
-  //   }
-  // };
-
  
   const handleLogin = async (event) => {
     event.preventDefault();
